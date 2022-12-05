88194a2c3cc5b16ac3d9feed99268f54d4332812
maxmattone <max.m@browserstack.com>
2022-12-05 11:26:41 +0000

    fixing selenium 4 deps

--- ios/testng-examples/src/test/java/com/browserstack/run_first_test/AppiumTest.java
+++ ios/testng-examples/src/test/java/com/browserstack/run_first_test/AppiumTest.java
@@ -2,11 +2,11 @@ package com.browserstack.run_first_test;
 
 import java.net.URL;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 
 
 public class AppiumTest {
@@ -15,8 +15,8 @@ public class AppiumTest {
 
     @BeforeMethod(alwaysRun=true)
     public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        XCUITestOptions options = new XCUITestOptions();
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),options);
     }
 
     @AfterMethod(alwaysRun=true)
