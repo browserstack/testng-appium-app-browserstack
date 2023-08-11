78e0a7656b686a823d9f02a739199f7fc79bcbc0
07souravkunda <souravkunda00@gmail.com>
2023-08-11 20:44:35 +0530

    update: appium driver to android driver

--- android/testng-examples/src/test/java/com/browserstack/AppiumTest.java
+++ android/testng-examples/src/test/java/com/browserstack/AppiumTest.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 
 public class AppiumTest {
 
-    public AppiumDriver driver;
+    public AndroidDriver driver;
 
     @BeforeMethod(alwaysRun=true)
     public void setUp() throws Exception {
