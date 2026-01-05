d4912b67392a93f97d4ec1fa0ffe321ffbb4a425
Rahul Singh <rahul.s@browserstack.com>
2026-01-05 14:04:13 +0530

    add diverse tests for ios and android

--- ios/testng-examples/src/test/java/com/browserstack/AppiumTest.java
+++ ios/testng-examples/src/test/java/com/browserstack/AppiumTest.java
@@ -1,9 +1,16 @@
 package com.browserstack;
 
 import java.net.URL;
+import java.time.Duration;
 
+import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import io.appium.java_client.ios.IOSDriver;
@@ -14,13 +21,16 @@ public class AppiumTest {
 
     public IOSDriver driver;
 
-    @BeforeMethod(alwaysRun=true)
+    @BeforeClass(alwaysRun=true)
     public void setUp() throws Exception {
         MutableCapabilities options = new XCUITestOptions();
         driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),options);
+        WebElement textButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Button")));
+        textButton.click();
     }
 
-    @AfterMethod(alwaysRun=true)
+    @AfterClass(alwaysRun=true)
     public void tearDown() throws Exception {
         driver.quit();
     }
---
+++ ios/testng-examples/src/test/java/com/browserstack/moduleb/DemoTest.java
@@ -0,0 +1,85 @@
+package com.browserstack.moduleb;
+
+import com.browserstack.AppiumTest;
+import com.browserstack.Retry;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DemoTest extends AppiumTest {
+
+    @Test(groups = {"regression"})
+    public void flakyTest_intermittentlyPassesAndFails() throws Exception {
+        WebElement textInput = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Input")));
+        String inputText = Math.random() > 0.7 ? "hello@browserstack.com" : "dlsjfkjlskjd";
+        textInput.sendKeys(inputText+"\n");
+
+        Thread.sleep(5000);
+
+        WebElement textOutput = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Output")));
+
+        Assert.assertEquals(textOutput.getText(),"hello@browserstack.com");
+    }
+
+    @Test
+    public void alwaysFailingTest_missingElement1() throws Exception {
+        throw new Exception("CustomException: Unable to locate element id xyz in app");
+    }
+
+    @Test(groups = {"regression", "p1"})
+    public void alwaysFailingTest_sameStacktrace1() throws Exception {
+        throw new Exception("InvalidParamsException: Received method signature does not match expected signature");
+    }
+
+    @Test
+    public void alwaysFailingTest_sameStacktrace2() throws Exception {
+        throw new Exception("InvalidParamsException: Received method signature does not match expected signature");
+    }
+
+    @Test(retryAnalyzer = Retry.class)
+    public void testWithFrameworkLevelRetry_2RetriesConfigured() {
+        Integer result = Math.random() > 0.7 ? 6 : 9;
+        try {
+            Assert.assertEquals((int) result, 6 + 3);
+        } catch (Exception e) {
+            throw new RuntimeException("Test failed. Will retry if more retries are configured, else will mark test as failed.");
+        }
+    }
+
+    @Test(retryAnalyzer = Retry.class)
+    public void anotherTestWithFrameworkLevelRetry_2RetriesConfigured() {
+        Integer result = Math.random() > 0.7 ? 6 : 9;
+        try {
+            Assert.assertEquals((int) result, 3 + 3);
+        } catch (Exception e) {
+            throw new RuntimeException("Test failed. Will retry if more retries are configured, else will mark test as failed.");
+        }
+    }
+
+    @Test
+    public void alwaysPassingTest_exampleA() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void alwaysPassingTest_exampleB() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void alwaysPassingTest_exampleC() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void alwaysPassingTest_exampleD() {
+        Assert.assertTrue(true);
+    }
+}
